66095aa40156e856cfee75d44077e53eda0360bb
MaeC <mae.cana@outlook.com>
2024-11-13 22:57:14 -0700

    Initial Scene + 2 boxGeo - Replicate initial scene - Added 2 boxGeometries - Style one with wireframe - Style one with Edges, and Line - Position both boxes on the left, and right of the IcosahedronGeometry

--- index.js
+++ index.js
@@ -1,68 +1,86 @@
-// Library Imports
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
-// Window Declarations
 const w = window.innerWidth;
 const h = window.innerHeight;
 
-// Three declarations
-const fov = 75;
+const fov = 45;
 const aspect = w / h;
 const near = 0.1;
 const far = 10;
 
-// Subject declaration
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const hemilight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+scene.add(hemilight);
+
+const geo = new THREE.IcosahedronGeometry(0.5, 2);
 const mat = new THREE.MeshStandardMaterial({
-    color: 0x06DEAE,
+    color: 0x06D995,
     flatShading: true,
 });
 const mesh = new THREE.Mesh(geo, mat);
 
-const wireframe = new THREE.MeshBasicMaterial({
+const wireframe = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     wireframe: true,
 });
+
 const wireframeMesh = new THREE.Mesh(geo, wireframe);
-wireframeMesh.scale.setScalar(1.001)
+wireframeMesh.scale.setScalar(1.001);
 mesh.add(wireframeMesh);
+scene.add(mesh);
 
-// 3 things needed for a Scene
-// 1. Renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
+const box = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const edges = new THREE.EdgesGeometry(box);
+const lineMat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+});
+const line = new THREE.LineSegments(edges, lineMat);
+const boxMat = new THREE.MeshStandardMaterial({
+    color: 0x06deae,
+    flatShading: true
+});
+const boxMesh = new THREE.Mesh(box, boxMat);
+boxMesh.position.x = 0.9;
+boxMesh.position.y = 0.2;
 
-// 2. Camera
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+scene.add(boxMesh);
+boxMesh.add(line);
 
-// 3. Scene
-const scene = new THREE.Scene();
+const box1 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const box1Mat = new THREE.MeshStandardMaterial({
+    color: 0x06feae,
+    flatShading: true,
+});
+const box1Mesh = new THREE.Mesh(box1, box1Mat);
+box1Mesh.position.x = -0.8;
 
-// Additional Scene Configuration
-// Lights
-const hemiLight = new THREE.HemisphereLight(0xfffff, 0x000000);
+const box1wireframe = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    wireframe: true
+});
+const box1wireframeMesh = new THREE.Mesh(box1, box1wireframe);
+box1wireframeMesh.scale.setScalar(1.001);
+box1Mesh.add(box1wireframeMesh);
+scene.add(box1Mesh);
 
-// Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.12;
 
-
-// Scene Assembly
-scene.add(mesh);
-scene.add(hemiLight);
-
-// Functions
-const animate = (t = 0) => {
+const animate = () => {
     requestAnimationFrame(animate);
-
     renderer.render(scene, camera);
     controls.update();
-}
+};
 
-// Render
 animate();
